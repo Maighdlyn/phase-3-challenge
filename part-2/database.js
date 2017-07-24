0b15dfd6230e4c271249b0ede2d171eb9c6beae6
maighdlyn <emmamaighdlyn@gmail.com>
2017-07-24 15:21:54 -0700

    Add itemsInSection() and corresponding test

--- part-2/database.js
+++ part-2/database.js
@@ -6,7 +6,7 @@ const db = pgp({
 
 const formatSpaces = (string, maxLength) => {
   if (string.length > maxLength) {
-    return string.slice(0,10) + "..."
+    return string.slice(0,maxLength) + "..."
   }
   else {
     while (string.length < maxLength + 3){
@@ -16,52 +16,73 @@ const formatSpaces = (string, maxLength) => {
   }
 }
 
-const queries = (query, parameter) => {
+const allItems = () => {
+    return db.any('SELECT * FROM items')
+      .then( (data) => {
+        const header = 'ID   Name          Price    Section\n-----------------------------------------\n'
+        const rows = data
+          .map(item => formatSpaces(item.id.toString(), 2) + formatSpaces(item.name, 10) + ' ' + formatSpaces(item.price, 5) + ' ' + item.section)
+          .join('\n')
+        return header + rows
+      })
+  }
+
+const itemsInSection = (parameter) => {
+    return db.any('SELECT * FROM items WHERE section = $1', [parameter])
+    .then( (data) => {
+      const header = 'ID   Name \n------------------\n'
+      const rows = data
+        .map(item => formatSpaces(item.id.toString(), 2) + item.name)
+        .join('\n')
+      return header + rows
+    })
+  }
+
+const checkNodeInput = (query, parameter) => {
   switch(query) {
     case 'allItems':
-      return db.any('SELECT * FROM items')
-        .then( (data) => {
-          // console.log(data)
-          const header = 'ID   Name          Price    Section\n-----------------------------------------\n'
-          const rows = data
-            .map(item => formatSpaces(item.id.toString(), 2) + formatSpaces(item.name, 10) + ' ' + formatSpaces(item.price, 5) + ' ' + item.section)
-            .join('\n')
-          return header + rows
-        })
-      break;
+      return queries.allItems()
+      break
+
     case 'itemsInSection':
-      console.log("These items are in " + parameter)
-      break;
+      return queries.itemsInSection(parameter)
+      break
+
     case 'cheapItems' :
       console.log("Items under $10")
-      break;
+      break
+
     case 'countItemsInSection' :
       console.log("count stuff in " + parameter)
-      break;
+      break
+
     case 'mostRecentOrders' :
       console.log("Recent orders")
-      break;
+      break
+
     case 'lastShopperName' :
       console.log("last shopper")
-      break;
+      break
+
     case 'orderTotal' :
       console.log("total for id " + parameter)
-      break;
+      break
   }
 }
 
 if(require.main === module) {
   const nodeQuery = process.argv[2]
   const nodeParameter = process.argv[3]
-  queries(nodeQuery, nodeParameter)
+  switchCheck(nodeQuery, nodeParameter)
     .then( (data) => {
       console.log(data)
       pgp.end()
     })
 }
 
+
 module.exports = {
-  // checkTest,
-  queries,
-  db
+  db,
+  allItems,
+  itemsInSection
 }
--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -1,14 +1,22 @@
 const expect = require('chai').expect
-const {checkTest, queries, db} = require('../database.js')
+const {db, allItems, itemsInSection} = require('../database.js')
 const pgPromise = require('pg-promise')
 const pgp = pgPromise()
 
 describe("Phase 3 Assessment, Part 2", () => {
-  it("'AllItems' should display a list of items", () => {
-    return queries('allItems')
+  it(' A call to itemsInSection("bulk") returns the items "Flour", "Pasta", and "Rice"', () => {
+    return itemsInSection('bulk')
       .then( (data) => {
         pgp.end()
-        expect(data.slice(0,188)).to.eql('ID   Name          Price    Section\n-----------------------------------------\n1    Aluminum F... 8.84     miscellaneous\n2    Apples        10.81    produce\n3    Bacon         9.01     meat')
+        expect(data).to.eql('ID   Name \n------------------\n16   Flour\n33   Pasta\n36   Rice')
       })
   })
+
+  // it('"AllItems" should display a list of items', () => {
+  //   return allItems()
+  //     .then( (data) => {
+  //       pgp.end()
+  //       expect(data.slice(0,188)).to.eql('ID   Name          Price    Section\n-----------------------------------------\n1    Aluminum F... 8.84     miscellaneous\n2    Apples        10.81    produce\n3    Bacon         9.01     meat')
+  //     })
+  // })
 })
