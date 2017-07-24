9a32f2d586fb75e73b71d08ca72f8a29aeefbf12
maighdlyn <emmamaighdlyn@gmail.com>
2017-07-24 09:46:52 -0700

    Setup routes with placeholder text

--- part-1/index.js
+++ part-1/index.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
   res.send('hello')
 })
 
+app.get('/supported-operations', (req, res) => {
+  res.send('supported-operations')
+})
+
+app.get('/square', (req, res) => {
+  res.send('square')
+})
+
+app.get('/compute', (req, res) => {
+  res.send('compute')
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log('Express server is listening on port', port)
