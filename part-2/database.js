31975d59a0d4c83a4b9f6249d225fffa48297886
maighdlyn <emmamaighdlyn@gmail.com>
2017-07-24 12:29:24 -0700

    Set up switch statement checking which query to run

--- part-2/database.js
+++ part-2/database.js
@@ -4,11 +4,40 @@ const db = pgp({
   database: 'grocery_store'
 })
 
-const checkTest = () => {
-  return 2
+// const checkTest = () => {
+//   return 2
+// }
+
+const queries = (query, parameter) => {
+  switch(query) {
+    case 'allItems':
+      console.log("All the items")
+      break;
+    case 'itemsInSection':
+      console.log("These items are in " + parameter)
+      break;
+    case 'cheapItems' :
+      console.log("Items under $10")
+      break;
+    case 'countItemsInSection' :
+      console.log("count stuff in " + parameter)
+      break;
+    case 'mostRecentOrders' :
+      console.log("Recent orders")
+      break;
+    case 'lastShopperName' :
+      console.log("last shopper")
+      break;
+    case 'orderTotal' :
+      console.log("total for id " + parameter)
+      break;
+  }
 }
+const nodeQuery = process.argv[2]
+const nodeParameter = process.argv[3]
+queries(nodeQuery, nodeParameter)
 
 module.exports = {
-  checkTest,
+  // checkTest,
   db
 }
--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -3,11 +3,11 @@ const {checkTest, db} = require('../database.js')
 const pgPromise = require('pg-promise')
 const pgp = pgPromise()
 
-describe("Checking that this test is connecting properly", () => {
-  it("2+2=4", () => {
-    expect(checkTest() + 2).to.eql(4)
-  })
-  it('2+2=3 should fail', () => {
-    expect(checkTest() +2).to.eql(3)
-  })
-})
+// describe("Checking that this test is connecting properly", () => {
+//   it("2+2=4", () => {
+//     expect(checkTest() + 2).to.eql(4)
+//   })
+//   it('2+2=3 should fail', () => {
+//     expect(checkTest() +2).to.eql(3)
+//   })
+// })
